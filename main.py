ec663f0f074c6d3f6b69e169361bc6acaefd898c
Mazurr <bartoszmazur98@gmail.com>
2020-10-03 18:57:00 +0200

    Add class to change views and prepare template for event view for given day

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
-import calendar as cl
+from datetime import date
+from views import CalendarView, DayEventsView
+from tkinter import Tk, Frame
 import calendar_db as cl_db
+
+class Views(Tk):
+    def __init__(self, *args, **kwargs):
+        Tk.__init__(self, *args, *kwargs)
+        self.current_view = None
+        day = date.today().day
+        month = date.today().month
+        year = date.today().year
+        self.view = {}
+        self.view["calendar_view"] = CalendarView
+        self.view["event_view"] = DayEventsView
+        
+        self.show_view("calendar_view", day=day, month=month, year=year)
+        del day, month, year
+
+    def show_view(self, page_name, **kwargs):
+        '''Show a view for the given view name'''
+        new_view = self.view[page_name](self, day=kwargs['day'], month=kwargs['month'], year=kwargs['year'])
+        
+        if self.current_view is not None:
+            self.current_view.destroy()
+
+        self.current_view = new_view
+        self.current_view.pack()
 
 if __name__ == '__main__':
     conn = cl_db.create_connection('cal.db')
     if conn:
-        app = cl.Calendar()
-        app.window.mainloop()
+        app = Views()
+        app.mainloop()
     conn.close()
--- views.py
+++ views.py
@@ -1,46 +1,42 @@
 from datetime import date, datetime
-from tkinter import Tk, Button, Label, Frame
+from tkinter import Button, Label, Frame
 
-class Calendar():
-    
-    def __init__(self):
-        l_mouth = range(1,32)
-        s_mouth = range(1,31)
-        self.calendar ={'1':('January',l_mouth), '2':['February',range(1,30)], '3':('March',l_mouth), '4':('April',s_mouth), '5':('May',l_mouth), 
-                        '6':('June',s_mouth), '7':('July',l_mouth), '8':('August',l_mouth), '9':('September',s_mouth), '10':('October',l_mouth),
-                        '11':('November',s_mouth), '12':('December',l_mouth)}
-        self.week = ('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
+class CalendarView(Frame):
+    l_mouth = range(1,32)
+    s_mouth = range(1,31)
+    calendar ={ '1':('January',l_mouth), '2':['February',range(1,30)], '3':('March',l_mouth), '4':('April',s_mouth), '5':('May',l_mouth), 
+                '6':('June',s_mouth), '7':('July',l_mouth), '8':('August',l_mouth), '9':('September',s_mouth), '10':('October',l_mouth),
+                '11':('November',s_mouth), '12':('December',l_mouth)}
+    week = ('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
 
-        self.window = Tk()
-        self.window.title("Calendar")
+    def __init__(self, window, **kwargs):
+        Frame.__init__(self, window)
+        self.window = window
         self.dt = date.today()
-        self.months = self.calendar[str(self.dt.month)]
-        self.month_n = self.dt.month
-        self.dt = date.today()
-        self.months = self.calendar[str(self.dt.month)]
-        self.month_n = self.dt.month
-        self.yr = self.dt.year
-        self.m_day_frame = Frame(self.window)
+        self.month_n = kwargs['month']
+        self.months = self.calendar[str(self.month_n)]
+        self.yr = kwargs['year']
+        self.m_day_frame = Frame(self)
         self.m_day_frame.grid(row=3, column=0, columnspan=7)
 
-        self.year = Label(self.window, text=self.yr, height=2, justify='center')
+        self.year = Label(self, text=self.yr, height=2, justify='center')
         self.year.grid(row = 0, column=1, columnspan=5)
 
-        self.y_button_left = Button(self.window, text="<", command=lambda: self.change_year(-1))
+        self.y_button_left = Button(self, text="<", command=lambda: self.change_year(-1))
         self.y_button_left.grid(row=0, column=0)
-        self.y_button_right = Button(self.window, text=">", command=lambda: self.change_year(1))
+        self.y_button_right = Button(self, text=">", command=lambda: self.change_year(1))
         self.y_button_right.grid(row=0, column=6)
 
-        self.l_month = Label(self.window, text=self.months[0], height=2, justify='center')
+        self.l_month = Label(self, text=self.months[0], height=2, justify='center')
         self.l_month.grid(row = 1, column=1, columnspan=5)
-        self.m_button_left = Button(self.window, text="<", command=lambda: self.change_month(-1))
+        self.m_button_left = Button(self, text="<", command=lambda: self.change_month(-1))
         self.m_button_left.grid(row=1, column=0)
-        self.m_button_right = Button(self.window, text=">", command=lambda: self.change_month(1))
+        self.m_button_right = Button(self, text=">", command=lambda: self.change_month(1))
         self.m_button_right.grid(row=1, column=6)
 
         i=0
         for w_day in self.week:
-            week_e = Label(self.window, text=w_day, width=15, height=2, justify='center')
+            week_e = Label(self, text=w_day, width=15, height=2, justify='center')
             week_e.grid(row=2, column=i)
             i += 1
         self.leap_year
@@ -92,7 +88,8 @@ class Calendar():
                 month_e = Label(self.m_day_frame, text=days-n+1, width=15, height=5, justify='center')
                 month_e.grid(row=r, column=c-n)
         for m_day in self.months[1]:
-            month_e = Button(self.m_day_frame, text=m_day, command=self.add_event, width=15, height=5, justify='center', bg=(today if m_day == self.dt.day else None ))
+            month_e = Button(self.m_day_frame, text=m_day, command=lambda: self.window.show_view("event_view", year=self.yr, month=self.month_n, day=m_day), 
+                            width=15, height=5, justify='center', bg=(today if m_day == self.dt.day else None ))
             month_e.grid(row=r, column=c)
             c += 1
             if(c == 7):
@@ -105,7 +102,16 @@ class Calendar():
 
     def get_first_day(self):
         return self.dt.replace(self.yr, month=self.month_n, day=1).weekday()
-    
-    def add_event(self):
-        pass
 
+class DayEventsView(Frame):
+    def __init__(self, window, **kwargs):
+        Frame.__init__(self, window)
+        self.day = kwargs['day']
+        self.month = kwargs['month']
+        self.year = kwargs['year']
+        self.year_l = Label(self, text=self.year, height=2, width=15, justify='center')
+        self.year_l.grid(row = 0, column=0)
+
+
+        month_e = Button(self, text="Cancel", command=lambda: window.show_view("calendar_view", year=self.year, month=self.month, day=self.day), width=15, height=2, justify='center')
+        month_e.grid(row=1, column=0)
