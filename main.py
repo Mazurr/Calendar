b5d9822ce411a63b602ef4255ca5d8fba17a15bb
Mazurr <bartoszmazur98@gmail.com>
2020-10-11 21:38:23 +0200

    Add all fetatures which was in Tkinter version except delete event

--- main.py
+++ main.py
@@ -1,25 +1,43 @@
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
+from kivy.uix.screenmanager import ScreenManager
 
 from datetime import date
 
-from views import CalendarView
+from views import CalendarView, AddEventView, EventListView
 from calendar_db import create_connection
 
-class CalendarApp(App):
-    def build(self):
+
+class ViewManager(ScreenManager):
+    day, month, year = 0, 0, 0
+
+    def __init__(self, conn):
+        super(ViewManager, self).__init__()
         dt = date.today()
-        day = dt.day
-        month = dt.month
-        year = dt.year
-        calendar = CalendarView(day=day, month=month, year=year)
-        del day, year, month
-        return calendar
+        self.conn = conn
+        self.day = dt.day
+        self.month = dt.month
+        self.year = dt.year
+        self.add_widget(CalendarView(day=self.day, month=self.month, year=self.year))
+        self.add_widget(AddEventView(conn=self.conn))
+        self.add_widget(EventListView(conn=self.conn))
+        self.change_screen(name="calendar")
+        del dt
+
+    def change_screen(self, name, **kwargs):
+        self.current = name
+
+
+class CalendarApp(App):
+    def __init__(self, conn):
+        super().__init__()
+        self.conn = conn
+
+    def build(self):
+        return ViewManager(conn=self.conn)
+
 
 if __name__ == "__main__":
     conn = create_connection("cal.db")
     if conn:
-        CalendarApp().run()
+        CalendarApp(conn=conn).run()
     conn.close()
--- views.py
+++ views.py
@@ -3,69 +3,79 @@ from datetime import date, datetime
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.widget import Widget
-from kivy.properties import StringProperty
+from kivy.uix.screenmanager import Screen
+from kivy.lang.builder import Builder
+from kivy.uix.gridlayout import GridLayout
 
-class CalendarView(Widget):
-    l_mouth = range(1, 32)
-    s_mouth = range(1, 31)
-    calendar = {
-        "1": ("January", l_mouth),
-        "2": ["February", range(1, 30)],
-        "3": ("March", l_mouth),
-        "4": ("April", s_mouth),
-        "5": ("May", l_mouth),
-        "6": ("June", s_mouth),
-        "7": ("July", l_mouth),
-        "8": ("August", l_mouth),
-        "9": ("September", s_mouth),
-        "10": ("October", l_mouth),
-        "11": ("November", s_mouth),
-        "12": ("December", l_mouth),
-    }
-    week = ("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
-    year_l = StringProperty()
-    month_l = StringProperty()
+calendar = {
+    "1": ("January", range(1, 32)),
+    "2": ("February", range(1, 30), range(1, 29)),
+    "3": ("March", range(1, 32)),
+    "4": ("April", range(1, 31)),
+    "5": ("May", range(1, 32)),
+    "6": ("June", range(1, 31)),
+    "7": ("July", range(1, 32)),
+    "8": ("August", range(1, 32)),
+    "9": ("September", range(1, 31)),
+    "10": ("October", range(1, 32)),
+    "11": ("November", range(1, 31)),
+    "12": ("December", range(1, 32)),
+}
+week = ("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
+
+
+class CalendarView(Screen):
     def __init__(self, **kwargs):
         super(CalendarView, self).__init__()
         self.dt = date.today()
         self.month_n = kwargs["month"]
-        self.month = self.calendar[str(self.month_n)]
-        self.month_l = self.month[0]
+        self.month = calendar[str(self.month_n)]
+        self.month_label.text = self.month[0]
         self.year = kwargs["year"]
-        self.year_l = str(kwargs["year"])
-        for w_day in self.week:
+        self.year_label.text = str(self.year)
+        for w_day in week:
             self.week_grid.add_widget(Label(text=w_day))
-        self.leap_year
         self.render_days()
 
     def render_days(self):
         self.days_grid.clear_widgets()
+        if self.month_n == 2:
+            l = self.leap_year()
+        else:
+            l = 1
         if self.month_n == 1:
             days = 31
         else:
-            days = ((self.calendar[str(self.month_n - 1)])[1])[-1]
+            days = ((calendar[str(self.month_n - 1)])[1])[-1]
+
         c = self.get_first_day()
         if c > 0:
             for n in range(c, 0, -1):
-                self.days_grid.add_widget(Label(text=str(days-n+1)))
+                self.days_grid.add_widget(Label(text=str(days - n + 1)))
+
         if self.year == self.dt.year and self.month_n == self.dt.month:
             today = [0.51, 0.89, 0.25, 0.65]
         else:
-            today = [1,1,1,1]
-        for m_day in self.month[1]:
-            self.days_grid.add_widget(Button(text=str(m_day), background_color=( today if m_day == self.dt.day else [1,1,1,1])))
+            today = [1, 1, 1, 1]
+        for m_day in self.month[l]:
+            self.days_grid.add_widget(
+                Button(
+                    text=str(m_day),
+                    background_color=(today if m_day == self.dt.day else [1, 1, 1, 1]),
+                    on_press=lambda d=m_day: self.add_event(d),
+                )
+            )
             c += 1
             if c >= 7:
                 c = 0
+
         if c < 7 and c != 0:
             for n in range(c, 7, 1):
                 self.days_grid.add_widget(Label(text=str(n - c + 1)))
-                
+
     def get_first_day(self):
         return self.dt.replace(self.year, month=self.month_n, day=1).weekday()
-    
+
     def change_year(self, k):
         if k == "<":
             self.year -= 1
@@ -84,18 +94,116 @@ class CalendarView(Widget):
             self.month_n = 12
         elif self.month_n > 12:
             self.month_n = 1
-        self.month = self.calendar[str(self.month_n)]
+        self.month = calendar[str(self.month_n)]
         self.month_label.text = self.month[0]
         self.render_days()
-    
+
     def leap_year(self):
         if (self.year % 4) == 0:
             if (self.year % 100) == 0:
                 if (self.year % 400) == 0:
-                    (self.calendar["2"])[1] = range(1, 30)
+                    return 1
                 else:
-                    (self.calendar["2"])[1] = range(1, 29)
+                    return 2
             else:
-                (self.calendar["2"])[1] = range(1, 30)
+                return 1
         else:
-            (self.calendar["2"])[1] = range(1, 29)
+            return 2
+
+    def add_event(self, day):
+        self.parent.year = self.year
+        self.parent.month = self.month_n
+        self.parent.day = int(day.text)
+        self.parent.change_screen("event_list")
+
+
+class EventListView(Screen):
+    def __init__(self, conn, **kwargs):
+        super(EventListView, self).__init__()
+        self.conn = conn
+        self.year = 2019
+        self.month = 20
+        self.day = 2
+
+    def on_pre_enter(self):
+        self.year = self.parent.year
+        self.month = self.parent.month
+        self.day = self.parent.day
+        self.year_label.text = str(self.year)
+        self.month_label.text = (calendar[str(self.month)])[0]
+        self.list_event()
+
+    def add_event(self):
+        self.parent.change_screen("add_event")
+
+    def go_back(self):
+        self.parent.change_screen("calendar")
+
+    def list_event(self):
+        self.event_list_grid.clear_widgets()
+        data = self.events_query()
+        for row in data:
+            self.event_list_grid.add_widget(Label(text=str(row)))
+            child_grid = GridLayout(
+                cols=2,
+                size_hint_y=None,
+                height=30,
+                row_default_height=30,
+                row_force_default=True,
+            )
+            child_grid.add_widget(Button(text="Edit"))
+            child_grid.add_widget(
+                Button(text="Delete", on_press=lambda id=row[0]: self.delete_event(id))
+            )
+            self.event_list_grid.add_widget(child_grid)
+
+    def delete_event(self, id):
+        c = self.conn.cursor()
+        print(id.on_press)
+        """
+        query = "DELETE FROM Events WHERE Events.id == {} ".format(id)
+        c.execute(query)
+        self.conn.commit()
+        """
+        self.parent.change_screen("event_list")
+
+    def events_query(self):
+        c = self.conn.cursor()
+        query = "SELECT * FROM Events WHERE Events.date == {:02}-{:02}-{:02} ORDER BY 'date'".format(
+            self.day, self.month, self.year
+        )
+        c.execute(query)
+        rows = c.fetchall()
+        return rows
+
+
+class AddEventView(Screen):
+    def __init__(self, conn, **kwargs):
+        super(AddEventView, self).__init__()
+        self.conn = conn
+        self.year = 2019
+        self.month = 20
+        self.day = 2
+
+    def on_pre_enter(self):
+        self.year = self.parent.year
+        self.month = self.parent.month
+        self.day = self.parent.day
+        self.year_label.text = str(self.year)
+        self.month_label.text = (calendar[str(self.month)])[0]
+
+    def go_back(self):
+        self.parent.change_screen("event_list")
+
+    def save_event(self):
+        title = self.title.text
+        descript = self.description.text
+        query = """ INSERT INTO Events(title, descript, date)
+              VALUES('{}', '{}', {:02}-{:02}-{:02}) """.format(
+            title, descript, self.day, self.month, self.year
+        )
+        print(query)
+        c = self.conn.cursor()
+        c.execute(query)
+        self.conn.commit()
+        self.go_back()
